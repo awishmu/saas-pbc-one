cfc2ca402fa715193b63ba5b5f36a1c32aa643b3
Awish Mu <61050376+awishmu@users.noreply.github.com>
2025-10-03 08:03:09 +0800

    basePath next.config.ts
    Update next.config.ts basePath

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next'
  
 const nextConfig: NextConfig = {
   /* config options here */
-  
+  basePath: 'https://awishmu.github.io/saas-pbc-one/',
   
   // Next.js.
   // https://github.com/shadcn-ui/ui/blob/078dfe66072c4ca780bbc99d4ad4b13b1f44fe7e/packages/shadcn/src/utils/get-project-info.ts#L71C1-L77C4%E2%80%9D

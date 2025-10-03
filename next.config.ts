e4976afc3542813be327126e4851c08c0e2773a9
Awish Mu <61050376+awishmu@users.noreply.github.com>
2025-10-03 10:58:26 +0800

    Update next.config.ts
    komentar */

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: 'https://awishmu.github.io/saas-pbc-one/',
-  
+  /*
   // Next.js.
   // https://github.com/shadcn-ui/ui/blob/078dfe66072c4ca780bbc99d4ad4b13b1f44fe7e/packages/shadcn/src/utils/get-project-info.ts#L71C1-L77C4%E2%80%9D
   if (configFiles.find((file) => file.startsWith("next.config."))?.length) {
@@ -13,6 +13,7 @@ const nextConfig: NextConfig = {
     type.isRSC = isUsingAppDir
     return type
   }
+  */
 }
  
 export default nextConfig

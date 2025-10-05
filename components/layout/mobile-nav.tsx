a6ebfb0a12d4bb77ecb917f8a9c9248eedeb7304
Awish Dot <awishdotid@gmail.com>
2025-10-05 12:41:44 +0800

    next auth 10/5/2025 12:41

--- components/layout/mobile-nav.tsx
+++ components/layout/mobile-nav.tsx
@@ -3,6 +3,6 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 import { useSelectedLayoutSegment } from "next/navigation";
 import { Menu, X } from "lucide-react";
-import { useSession } from "next-auth/react";
+// import { useSession } from "next-auth/react";
 
 import { docsConfig } from "@/config/docs";

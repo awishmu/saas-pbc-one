c0ee24000e422ba4fada33370b0f1285f2884cbe
Awish Dot <awishdotid@gmail.com>
2025-10-05 12:39:24 +0800

    menu nav 10/5/2025 12:39

--- components/layout/mobile-nav.tsx
+++ components/layout/mobile-nav.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import { useSelectLayoutSegment } from "next/navigation";
+import { useSelectedLayoutSegment } from "next/navigation";
 import { Menu, X } from "lucide-react";
 import { useSession } from "next-auth/react";
 

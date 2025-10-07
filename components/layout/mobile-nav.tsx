df1271da8356a50c6260b62fd1cc3a52e21b26ea
Awish Dot <awishdotid@gmail.com>
2025-10-07 22:07:00 +0800

    mobile nav 10/7/2025 22:06

--- components/layout/mobile-nav.tsx
+++ components/layout/mobile-nav.tsx
@@ -9,8 +9,43 @@ import { docsConfig } from "@/config/docs";
 import { marketingConfig } from "@/config/marketing";
 import { siteConfig } from "@/config/site";
 import { cn } from "@/lib/utils";
-// import { DocsSidebarNav }
+import { DocsSidebarNav } from "@/components/docs/sidebar-nav";
+import { Icons } from "@/components/shared/icons";
+
+import { ModeToggle} from "./mode-toggle"
 
 export function NavMobile() {
-    //
+    // const { data: session } = useSession();
+    const [ open, setOpen ] = useState(false);
+    const selectedLayout = useSelectedLayoutSegment();
+    const documentation = selectedLayout === "docs";
+
+    const configMap = {
+        docs: docsConfig.mainNav,
+    };
+
+    const links = (selectedLayout && configMap[selectedLayout]) || marketingConfig.mainNav;
+
+    // prevent body scroll when modal is open
+    useEffect(() => {
+        if (open) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "auto";
+        }
+    }, [open]);
+
+    return (
+        <>
+            <button 
+                onClick={() => setOpen(!open)}
+                className={cn(
+                    "fixed right-2 top-2.5 "
+                )}
+            >
+
+
+            </button>
+        </>
+    )
 }

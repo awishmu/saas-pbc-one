e67d8bc07bcfe23ffa1288aafa6e2323a85a0742
Awish Dot <awishdotid@gmail.com>
2025-10-08 09:12:12 +0800

    mobile-nav 10/8/2025 9:12

--- components/layout/mobile-nav.tsx
+++ components/layout/mobile-nav.tsx
@@ -40,12 +40,97 @@ export function NavMobile() {
             <button 
                 onClick={() => setOpen(!open)}
                 className={cn(
-                    "fixed right-2 top-2.5 "
+                    "fixed right-2 top-2.5 z-50 rounded-full p-2 transition-colors duration-200 hover:bg-muted focus:outline-none active:bg-muted md:hidden",
+                    open && "hover:bg-muted active:bg-muted",
                 )}
             >
-
-
+                {open ? (
+                    <X className="size-5 text-muted-foreground" />
+                ) : (
+                    <Menu className="size-5 text-muted-foreground" />
+                )}
             </button>
+
+            <nav
+                className={cn(
+                    "fixed inset-0 z-20 hidden w-full overflow-auto bg-background px-5 py-16 lg:hidden",
+                    open && "block"
+                )}
+            >
+                <ul className="grid divide-y divide-muted">
+                    {links && links.length > 0 && links.map(({ title, href }) => (
+                        <li key={href} className="py-3">
+                            <Link
+                                href={href}
+                                onClick={() => setOpen(false)}
+                                className="flex w-full font-medium capitalize"
+                            >
+                                {title}
+                            </Link>
+                        </li>
+                    ))}
+
+                    {/* 
+                    {session ? (
+                        <>
+                            {session.user.role === "ADMIN" ? (
+                                <li className="py-3">
+                                    <Link>
+                                        Adminhg
+                                    </Link>
+                                </li>
+                            ) : null}
+
+                            <li className="py-3">
+                                <Link 
+                                    href="/dashboard"
+                                    onClick={() => setOpen(false)}
+                                    className="flex w-full font-medium capitalize"
+                                >
+                                    Dashboard
+                                </Link>
+                            </li>
+                        </>
+                    ) : (
+                        <>
+                            <li className="py-3">
+                                <Link
+                                href="/login"
+                                onClick={() => setOpen(false)}
+                                className="flex w-full font-medium capitalize"
+                                >
+                                Login
+                                </Link>
+                            </li>
+
+                            <li className="py-3">
+                                <Link
+                                href="/register"
+                                onClick={() => setOpen(false)}
+                                className="flex w-full font-medium capitalize"
+                                >
+                                Sign up
+                                </Link>
+                            </li>
+                        </>
+                    )}
+                    */}
+                </ul>
+                
+                    {documentation ? (
+                        <div className="mt-8 block md:hidden">
+                            <DocsSidebarNav setOpen={setOpen} />
+                        </div>
+                    ) : null}
+
+                    <div className="mt-5 flex items-center justify-end space-x-4">
+                        <Link href={siteConfig.links.github} target="_blank" rel="noreferrer">
+                            <Icons.gitHub className="size-6" />
+                            <span className="sr-only">GitHub</span>
+                        </Link>
+                        <ModeToggle />
+                    </div>
+            </nav>
         </>
     )
 }

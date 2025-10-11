3aeff2067fd1ebac957c256f4b0d82f5c390d6c8
Awish Dot <awishdotid@gmail.com>
2025-10-11 08:13:16 +0800

    navbar 10/11/2025 8:13

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -10,8 +10,12 @@ import { marketingConfig } from "@/config/marketing";
 import { siteConfig } from "@/config/site";
 import { cn } from "@/lib/utils";
 import { useScroll } from "@/hooks/use-scroll";
-
-
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+import { DocsSearch } from "@/components/docs/search";
+import { ModalContext } from "@/components/modals/provider";
+import { Icons } from "@/components/shared/icons";
+import MaxWidthWrapper from "@/components/shared/max-width/wrapper";
 
 interface NavBarProps {
     scroll?: boolean;
@@ -19,5 +23,22 @@ interface NavBarProps {
 }
 
 export function NavBar({ scroll = false }: NavBarProps) {
-    //
+    const scrolled = useScroll(50);
+	//const { data: session, status } = useSession();
+	const { setShowSignInModal} = useContext(ModalContext);
+	
+	const selectedlayout = useSelectedLayoutSegment();
+	const documentation = selectedLayout === "docs";
+	
+	const configMap = {
+		docs: docsConfig.mainNav,
+	}
+	
+	const links = (selectedlayout && configMap[selectedLayout]) || marketingConfig.mainNav;
+	
+	return (
+		<header></header>
+	)
+	
+	
 }

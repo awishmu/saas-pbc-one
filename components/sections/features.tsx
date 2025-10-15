b8e09a06527c228cb226dfc953d928ef100905ee
Awish Dot <awishdotid@gmail.com>
2025-10-15 12:30:50 +0800

    features 10/15/2025 12:30 am

--- components/sections/features.tsx
+++ components/sections/features.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 import { features } from "@/config/landing";
-import { Button } from "@/components/ui/buttons";
+import { Button } from "@/components/ui/button";
 import { HeaderSection } from "@/components/shared/header-section";
 import { Icons } from "@/components/shared/icons";
 import MaxWidthWrapper from "@/components/shared/max-width-wrapper";
@@ -9,6 +9,15 @@ import MaxWidthWrapper from "@/components/shared/max-width-wrapper";
 export default function Features() {
 	return (
 		<section>
+			<div className="pb-6 pt-28">
+				<MaxWidthWrapper>
+					<HeaderSection
+						label="Features"
+						title="Discover all features"
+						subtitle="Harum quae dolore invenmtore repuidandae? rorrup aut teporibus aiarutur."
+					/>
+				</MaxWidthWrapper>			
+			</div>
 		</section>
 	)
 }

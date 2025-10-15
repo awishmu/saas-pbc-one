84c2e24f384fa54b6c88dce52dd7d71202183947
Awish Dot <awishdotid@gmail.com>
2025-10-15 08:52:34 +0800

    features 10/15/2025 8:52 am

---
+++ components/sections/features.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+import { features } from "@/config/landing";
+import { Button } from "@/components/ui/buttons";
+import { HeaderSection } from "@/components/shared/header-section";
+import { Icons } from "@/components/shared/icons";
+import MaxWidthWrapper from "@/components/shared/max-width-wrapper";
+
+export default function Features() {
+	return (
+		<section>
+		</section>
+	)
+}
---
+++ components/shared/header-section.tsx
@@ -0,0 +1,11 @@
+interface HeaderSectionPages {
+	label?: string;
+	title?: string;
+	subtitle?: string,
+}
+
+export function HeaderSection({ label, title, subtitle }: HeaderSectionPorps) {
+	return (
+		
+	)
+}
